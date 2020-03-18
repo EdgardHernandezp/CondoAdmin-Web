2f81212eb36e6704143b3388a4f096364a1cd3a1
Dreamseeker5 <edgardhernandezp@gmail.com>
2020-03-17 21:02:06 -0300

    First commit.

---
+++ src/main/java/com/dreamseeker/CondoAdminWebApplication.java
@@ -0,0 +1,13 @@
+package com.dreamseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondoAdminWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondoAdminWebApplication.class, args);
+	}
+
+}
