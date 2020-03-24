ce4c482149b1cf0a41e44d4d32223afd3f06b120
Dreamseeker5 <edgardhernandezp@gmail.com>
2020-03-24 11:22:56 -0300

    Added handlers to be able to perform the "search by name". Currently, is just displaying the name and last name inputs in the result page.

---
+++ src/main/java/com/dreamseeker/controllers/IndexController.java
@@ -0,0 +1,40 @@
+package com.dreamseeker.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.dreamseeker.models.dao.DwellingRepository;
+import com.dreamseeker.models.entity.Dwelling;
+import com.dreamseeker.models.services.DAOService;
+
+@Controller
+public class IndexController {
+	
+	@Autowired
+	DAOService daoService;
+		
+	@GetMapping("/searchByName")
+	public String greeting() {
+		return "searchByName";
+	}
+	
+	@PostMapping("/searchByName")
+	public String getSearchResults(String name, String lastName) {
+		List<Dwelling> dwellings = daoService.getDwellingByName(name, lastName); 
+		return "redirect:/result?name="+name+"&lastName="+lastName;
+	}
+	
+	@GetMapping("/result")
+	public String showResults(@RequestParam("name") String name, @RequestParam("lastName") String lastName, Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("lastName", lastName);
+		return "result";
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+--INSERT INTO Person (id, name, lastname, dni, apartmentID) VALUES (1, 'Edgar', 'Hernandez', '26659514-k', '701');
+--INSERT INTO Person (id, name, lastname, dni, apartmentID) VALUES (2, 'David', 'Parra', '24554456-k', '512');
+
+INSERT INTO Dwelling (id, name, lastname, dni, floor, apartmentID, debt) VALUES (1, 'Edgar', 'Hernandez', '26659514-k', 5, '701', 90000);
+INSERT INTO Dwelling (id, name, lastname, dni, floor, apartmentID, debt) VALUES (2, 'David', 'Parra', '24554456-k', 5, '504', 45000);
