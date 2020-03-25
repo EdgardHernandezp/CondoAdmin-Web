87691a393d46c19fd408721dd71957e7d76ab18d
Dreamseeker5 <edgardhernandezp@gmail.com>
2020-03-25 20:31:43 -0300

    Added a view for searching by DNI

--- src/main/java/com/dreamseeker/controllers/IndexController.java
+++ src/main/java/com/dreamseeker/controllers/IndexController.java
@@ -1,16 +1,17 @@
 package com.dreamseeker.controllers;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.dreamseeker.models.dao.DwellingRepository;
 import com.dreamseeker.models.entity.Dwelling;
 import com.dreamseeker.models.services.DAOService;
 
@@ -21,14 +22,30 @@ public class IndexController {
 	DAOService daoService;
 		
 	@GetMapping("/searchByName")
-	public String greeting() {
+	public String getByName() {
 		return "searchByName";
 	}
 	
 	@PostMapping("/searchByName")
-	public String getSearchResults(String name, String lastName) { 
+	public String redirectNameResults(String name, String lastName) { 
 		return "redirect:/result?name="+name+"&lastName="+lastName;
 	}
+	
+	
+	@GetMapping("/searchByDni")
+	public String getByApartmentId() {
+		return "searchByDni";
+	}
+	
+	@PostMapping("/searchByDni")
+	public String getByApartment(String dni, RedirectAttributes redirectAttr) {
+		List<Dwelling> dwellings = new ArrayList<Dwelling>();
+		dwellings.add(daoService.getDwellingByDNI(dni));
+		
+		redirectAttr.addFlashAttribute("dwellings", dwellings);
+		return "redirect:/result";
+	}
+
 	
 	@GetMapping("/result")
 	public String showResults(@RequestParam("name") String name, @RequestParam("lastName") String lastName, Model model) {
